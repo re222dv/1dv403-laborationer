cd6db82bfc089eb5e813124de0367e6b75a3a2bd
Rasmus Eneman <re222dv@student.lnu.se>
2013-11-15 11:31:29 +0100

    Start working on Uppgift 4 - Vandalen

--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -1,9 +1,20 @@
 "use strict";
 
 var makePerson = function(persArr){
+    var total_age = 0;
+    var max_age = 0;
+    var min_age = Infinity;
+    var names = [];
 
+    persArr.forEach(function(person) {
+        names.push(person.name);
+        total_age += person.age;
+        max_age = max_age > person.age ? max_age : person.age;
+        min_age = min_age < person.age ? min_age : person.age;
+    });
 
-	// Din kod här...
+    var average_age = Math.round(total_age / persArr.length);
 
+    return {minAge: min_age, maxAge: max_age, averageAge: average_age, names: names};
 }
 
