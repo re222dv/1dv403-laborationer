fd009aeb06c7963dc383c67c85c2cf66d992d70b
re222dv <re222dv@student.lnu.se>
2013-11-21 15:17:18 +0100

    Prototype Message

--- 2-labbymezzage/mezzage.js
+++ 2-labbymezzage/mezzage.js
@@ -1,3 +1,34 @@
+function Message(message, date) {
+
+    this.getText = function() {
+        return message;
+    }
+
+    this.setText = function (value) {
+        message = value;
+    }
+
+    this.getDate = function () {
+        return date;
+    }
+
+    this.setDate = function (value) {
+        date = value;
+    }
+}
+
+Message.prototype.toString = function() {
+    return this.getText() + ' (' + this.getDate() + ')';
+}
+
+Message.prototype.getHtmlText = function () {
+    return this.getText().replace('\n', '<br />');
+}
+
+Message.prototype.getDateText = function () {
+    return this.getDate();
+}
+
 function LabbyMessage() {
 }
 
