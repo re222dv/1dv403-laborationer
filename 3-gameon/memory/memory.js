3587ad5a0a55e3adedd3950737ae1540f15c8e88
Rasmus Eneman <re222dv@student.lnu.se>
2013-11-30 16:00:14 +0100

    Draw fronts

--- 3-gameon/memory/memory.js
+++ 3-gameon/memory/memory.js
@@ -22,11 +22,17 @@ Memory.prototype.init = function() {
 
         for (var col = 0; col < this.cols; col++) {
             var td = document.createElement('td');
+            var a = document.createElement('a');
             var img = document.createElement('img');
+            var img2 = document.createElement('img');
 
+            a.setAttribute('href', '#');
             img.setAttribute('src', 'pics/0.png');
+            img2.setAttribute('src', 'pics/' + this.board[row*4 + col] + '.png');
 
-            td.appendChild(img);
+            a.appendChild(img);
+            a.appendChild(img2);
+            td.appendChild(a);
             tr.appendChild(td);
         }
         table.appendChild(tr);
