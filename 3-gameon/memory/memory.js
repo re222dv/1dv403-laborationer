b0950ef349dbfadc9871381cafc770123691a689
Rasmus Eneman <re222dv@student.lnu.se>
2013-11-30 15:54:59 +0100

    Draw game board

--- 3-gameon/memory/memory.js
+++ 3-gameon/memory/memory.js
@@ -1,15 +1,39 @@
 "use strict"
 
-function Memory(id) {
+function Memory(id, rows, cols) {
     var self = this;
-    var board = [];
+    this.rows = rows;
+    this.cols = cols;
+
+    this.board = [];
+
+    this.node = document.getElementById(id);
 
     this.init();
 }
 
 Memory.prototype.init = function() {
+    this.board = RandomGenerator.getPictureArray(this.rows, this.cols);
+
+    var table = document.createElement('table');
+
+    for (var row = 0; row < this.rows; row++) {
+        var tr = document.createElement('tr');
+
+        for (var col = 0; col < this.cols; col++) {
+            var td = document.createElement('td');
+            var img = document.createElement('img');
+
+            img.setAttribute('src', 'pics/0.png');
+
+            td.appendChild(img);
+            tr.appendChild(td);
+        }
+        table.appendChild(tr);
+    }
+    this.node.appendChild(table);
 };
 
 window.onload = function() {
-    new Memory('memory');
+    new Memory('memory', 4, 4);
 };
