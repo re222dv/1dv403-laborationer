6237fe48780baccab90461dae361254dce0f992a
Rasmus Eneman <re222dv@student.lnu.se>
2013-12-28 09:40:40 -0600

    Start working on Pictures

---
+++ 5-pwd/applications/pictures.js
@@ -0,0 +1,27 @@
+"use strict"
+
+var RWWM = RWWM || {};
+RWWM.applications = RWWM.applications || {};
+RWWM.applications.pictures = {};
+
+RWWM.applications.pictures.Pictures = function() {
+    var that = this;
+    RWWM.Window.call(this, 400, 500, "Pictures", "");
+
+    var timeout = window.setTimeout(function() {
+        that.setStatusLoading();
+    }, 100);
+
+    var start = Date.now();
+    $.ajax("http://homepage.lnu.se/staff/tstjo/labbyServer/imgviewer/", {"dataType": "json", "success": function(data) {
+        var end = Date.now();
+        window.clearTimeout(timeout);
+        that.setStatus(data.length + ' pictures loaded in ' + ((end - start) / 1000).toFixed(1) + ' seconds');
+    }});
+
+};
+
+RWWM.applications.pictures.Pictures.prototype = Object.create(RWWM.Window.prototype);
+RWWM.applications.pictures.Pictures.prototype.constructor = RWWM.applications.pictures.Pictures;
+
+RWWM.launcher.add("Pictures", "", RWWM.applications.pictures.Pictures);
