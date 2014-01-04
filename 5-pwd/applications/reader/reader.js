24c26b25b42822220055972c5df648cfb8075f5f
Rasmus Eneman <pie.or.paj@gmail.com>
2014-01-04 13:45:14 +0100

    Code cleanup

--- 5-pwd/applications/reader/reader.js
+++ 5-pwd/applications/reader/reader.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 var RWWM = RWWM || {};
 RWWM.applications = RWWM.applications || {};
@@ -6,18 +6,19 @@ RWWM.applications.reader = {};
 
 RWWM.applications.reader.Reader = function() {
     var that = this;
-    RWWM.Window.call(this, 400, 500, "Reader", "applications/reader/icon.png");
+    RWWM.Window.call(this, 400, 500, 'Reader', 'applications/reader/icon.png');
 
-    this.container.classList.add("reader");
+    this.container.classList.add('reader');
 
     var timeout = window.setTimeout(function() {
         that.setStatusLoading();
     }, 100);
 
-    $(this.view).load("http://homepage.lnu.se/staff/tstjo/labbyServer/rssproxy/?url=" +
-        encodeURIComponent("http://www.dn.se/m/rss/senaste-nytt"), function() {
+    $(this.view).load('http://homepage.lnu.se/staff/tstjo/labbyServer/rssproxy/?url=' +
+        encodeURIComponent('http://www.dn.se/m/rss/senaste-nytt'), function() {
 
         var now = new Date();
+        window.clearTimeout(timeout);
         that.setStatus('Last updated ' + ('00' + now.getHours()).slice(-2) + ':' + ('00' + now.getMinutes()).slice(-2));
     });
 
@@ -26,4 +27,4 @@ RWWM.applications.reader.Reader = function() {
 RWWM.applications.reader.Reader.prototype = Object.create(RWWM.Window.prototype);
 RWWM.applications.reader.Reader.prototype.constructor = RWWM.applications.reader.Reader;
 
-RWWM.launcher.add("Reader", "applications/reader/icon.png", RWWM.applications.reader.Reader);
+RWWM.launcher.add('Reader', 'applications/reader/icon.png', RWWM.applications.reader.Reader);
