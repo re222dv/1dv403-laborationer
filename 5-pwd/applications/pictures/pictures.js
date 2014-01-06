657498d980d3332473348f7c54efe80d418501ad
Rasmus Eneman <re222dv@student.lnu.se>
2014-01-06 13:56:45 +0100

    Pictures: calculate the zoom percent onresize also workarond a bug in chrome with background-size: cover

--- 5-pwd/applications/pictures/pictures.js
+++ 5-pwd/applications/pictures/pictures.js
@@ -5,10 +5,22 @@ RWWM.applications = RWWM.applications || {};
 RWWM.applications.pictures = {};
 
 RWWM.applications.pictures.Picture = function(image) {
-    RWWM.Window.call(this, image.width, image.height, 'Picture', 'applications/pictures/icon.png');
+    var width = image.width;
+    var height = image.height;
+
+    // Workaround what looks like a bug in Chrome
+    if (width > height) {
+        height -= 1;
+    } else {
+        width -= 1;
+    }
+
+    RWWM.Window.call(this, width, height, 'Picture', 'applications/pictures/icon.png');
 
     this.container.classList.add('picture');
 
+    this.imageWidth = image.width;
+    this.imageHeight = image.height;
     this.setStatus(image.width + ' x ' + image.height + ' 100%');
 
     this.view.style.backgroundImage = 'url("' + image.URL + '")';
@@ -16,6 +28,21 @@ RWWM.applications.pictures.Picture = function(image) {
 
 RWWM.applications.pictures.Picture.prototype = Object.create(RWWM.Window.prototype);
 RWWM.applications.pictures.Picture.prototype.constructor = RWWM.applications.pictures.Picture;
+
+RWWM.applications.pictures.Picture.prototype.onresize = function() {
+    var percent = undefined;
+
+    var width = $(this.view).width();
+    var height = $(this.view).height();
+
+    if (width / this.imageWidth < height / this.imageHeight) {
+        percent = (width - this.imageWidth) / this.imageWidth;
+    } else {
+        percent = (height - this.imageHeight) / this.imageWidth;
+    }
+
+    this.setStatus(this.imageWidth + ' x ' + this.imageHeight + ' ' + Math.round((1 + percent) * 100) + '%');
+};
 
 RWWM.applications.pictures.Pictures = function() {
     var that = this;
